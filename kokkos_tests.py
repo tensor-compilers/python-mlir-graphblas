f3dd22fcd478cb0d621bf902801d9d1de7cb6e83
Ali Khan <76632881+ayk314@users.noreply.github.com>
2024-04-18 13:20:48 -0600

    Support for multi ops and trials and saving times

--- kokkos_tests.py
+++ kokkos_tests.py
@@ -3,6 +3,7 @@ from mlir_graphblas import tensor, operators, operations, io
 import argparse
 import numpy as np
 from timeit import default_timer
+import csv
 
 
 def setup_tensor(mtxFile, matrix=True, sparse=True):
@@ -31,10 +32,12 @@ def vxv(a, b, index_instance=0, num_instances=1, options=""):
     operations.ewise_mult(c, operators.BinaryOp.times, a, b, index_instance=index_instance, num_instances=num_instances, options=options)
     return c
 
+
 def vxm(a, B, index_instance=0, num_instances=1, options=""):
     c = tensor.Vector.new(B.dtype, B.shape[1])
     operations.vxm(c,operators.Semiring.plus_times, a, B, index_instance=index_instance, num_instances=num_instances, options=options)
     return c
+
 
 def mxv(A, b, index_instance=0, num_instances=1, options=""):
     c = tensor.Vector.new(A.dtype, A.shape[0])
@@ -90,23 +93,25 @@ def mxvxv(A, b, c, index_instance=0, num_instances=2, options=""):
 
 
 def main():
-    operations_dict = {"v+v":(vecadd,'v','v'),
-                       "vxv":(vxv,'v','v'),
-                       "vxm":(vxm,'v','m'),
-                       "mxv":(mxv,'m','v'),
-                       "m+m":(matadd,'m','m'),
-                       "mxm":(mxm,'m','m'),
-                       "v+v+v":(v_plus_v_plus_v,'v','v','v'),
-                       "vxvxv":(vxvxv,'v','v','v'),
-                       "mxmxm":(mxmxm,'m','m','m'),
-                       "mxvxv":(mxvxv,'m','v','v')}
+    operations_dict = {"v+v":(vecadd,('v','v')),
+                       "vxv":(vxv,('v','v')),
+                       "vxm":(vxm,('v','m')),
+                       "mxv":(mxv,('m','v')),
+                       "m+m":(matadd,('m','m')),
+                       "mxm":(mxm,('m','m')),
+                       "v+v+v":(v_plus_v_plus_v,('v','v','v')),
+                       "vxvxv":(vxvxv,('v','v','v')),
+                       "mxmxm":(mxmxm,('m','m','m')),
+                       "mxvxv":(mxvxv,('m','v','v'))}
     parser = argparse.ArgumentParser(prog="KokkosTest",description="For testing the Python-MLIR-GraphBLAS-Kokkos pipeline")
     parser.add_argument("mtxFile", help="Path to input matrix or vector in Matrix Market file format")
-    parser.add_argument("operation", choices=list(operations_dict.keys()), help="Operation to perform")
+    parser.add_argument("operations", choices=list(operations_dict.keys()), nargs="+", help="Operation(s) to perform")
     parser.add_argument("runtime", choices=["serial","openmp","gpu"], help="Computational runtime. Supports single-node serial, openmp, or gpu modes")
     parser.add_argument("-v","--verbose", action="store_true", help="Increase verbosity")
     parser.add_argument("--mtxout", help="Path to output Matrix Market file format. Note that this can be slow for large dense matrices")
     parser.add_argument("-s","--sparsity", choices=["sparse","dense"], default="sparse", help="Sparsity of the operands of either sparse or dense")  # TODO: mixed
+    parser.add_argument("-n","--trials", type=int, default=1, help="Number of trials to perform per operation")
+    parser.add_argument("--timesout", help="Path to output csv of execution times")
     # parser.add_argument("--validate", action="store_true", help="Validate result against equivalent numpy operation (Note slow for large matrices)")
     args = parser.parse_args()
 
@@ -116,11 +121,19 @@ def main():
         options="parallelization-strategy=any-storage-any-loop kokkos-uses-hierarchical"
     else:
         options = ""
-    operation_func,*operand_types = operations_dict[args.operation]
+    
+    exec_times = []
+    operations = {operation:operations_dict[operation] for operation in args.operations}
 
-    contains_matrix_operation = True if 'm' in operand_types else False
+    contains_matrix_operation = False
+    contains_vector_operation = False
+    for operation_func, operand_types in operations.values():
+        if 'm' in operand_types:
+            contains_matrix_operation = True
+        if 'v' in operand_types:
+            contains_vector_operation = True    
+
     is_dense = True if args.sparsity == "dense" else False
-    operands = []
 
     timer = default_timer()
     if contains_matrix_operation:
@@ -133,32 +146,51 @@ def main():
         print(f"Shape={vector.shape}")
     print(f"Finished reading mtx in {(default_timer() - timer):.4f} seconds")
 
-    if contains_matrix_operation and 'v' in operand_types:
+    # if contains_matrix_operation and 'v' in operand_types:
+    if contains_matrix_operation and contains_vector_operation:
         print("Mixed tensor types, reading mtx as a vector too")
         timer2 = default_timer()
         vector  = setup_tensor(mtxFile=args.mtxFile,matrix=False, sparse=not is_dense)
         print(f"Shape={vector.shape}")
         print(f"Finished reading mtx in {(default_timer() - timer2):.4f} seconds")
     
-    for operand in operand_types:
-        if operand == 'm':
-            operands.append(matrix)
-        else:
-            operands.append(vector)
     print("Finished preprocessing")
-
-    print("Starting excecuting of", args.operation)
-    timer2 = default_timer()
-    out_tensor = operation_func(*operands, options=options)
-    print(out_tensor.extract_tuples())
-    print("Finished excecuting of operation")
-    print(f"Time elapsed for execution (including compilation): {(default_timer() - timer2):.4f} seconds")
-    if args.mtxout:
-        print(f"Writing output to {args.mtxout}")
-        timer2 = default_timer()
-        io.to_mtx(args.mtxout, out_tensor)
-        print(f"Finished writing tensor in {(default_timer() - timer2):.4f} seconds")
+    index_instance = 0
+    num_instances = len(operations)
+    for operation, operation_info in operations.items():
+        operation_func, operand_types = operation_info
+        operands = []
+        for operand in operand_types:
+            if operand == 'm':
+                operands.append(matrix)
+            else:
+                operands.append(vector)
+        print("Starting excecution of", operation)
+        for iteration in range(1,args.trials+1):
+            print(f"Iteration {iteration}/{args.trials} of {operation}")
+            timer2 = default_timer()
+            print(operation,operation_func,operands)
+            out_tensor = operation_func(*operands, index_instance=index_instance, num_instances=num_instances, options=options)
+            elapsed = default_timer() - timer2
+            exec_times.append([operation,iteration,elapsed])
+            print(f"Time elapsed for execution (my include compilation): {elapsed:.4f} seconds")
+        if args.verbose:
+            print(out_tensor.extract_tuples())
+        print("Finished excecuting of operation")
+        if args.mtxout:
+            print(f"Writing output to {args.mtxout}")
+            timer2 = default_timer()
+            io.to_mtx(args.mtxout, out_tensor)
+            print(f"Finished writing tensor in {(default_timer() - timer2):.4f} seconds")
+        index_instance += 1
     print(f"Total time elapsed: {(default_timer() - timer):.4f} seconds")
+    if args.timesout:
+        with open(args.timesout, 'w', newline='') as csvfile:
+            timeswriter = csv.writer(csvfile)
+            timeswriter.writerow(["Operation","Iteration","Execution Time in seconds"])  # header
+            for row in exec_times:
+                timeswriter.writerow(row)
+        print("Finished Writing execution times to", args.timesout)
     print("Done.")
 
 
